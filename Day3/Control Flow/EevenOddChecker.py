1ebc3978da3f391ce2f088bdb09f9ad06587319c
Tochee <onyemstochi3@gmail.com>
2023-06-26 17:17:25 +0100

    Day 3

---
+++ Day3/Control Flow/EevenOddChecker.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 1:
+    print(str(number))
+    print("number is an odd number.")
+else:
+    print(str(number))
+    print("number is an even number.")
