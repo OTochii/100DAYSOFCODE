00bacd69f6b5cc8afb8dec5f3422cedd4f3164fb
Tochee <onyemstochi3@gmail.com>
2023-06-26 17:15:34 +0100

    Day 1

---
+++ Day1/WorkingWithVariables.py
@@ -0,0 +1,46 @@
+#String Manupilation
+
+#Thorny app helps debug code step-by-step
+
+#Concatenate Strings
+# print("Concatenate Strings.")
+# print("Hello" + " World")
+# print("Hello " + "World")
+# print("Hello" + " " + "World")
+
+#Output multiple Strings Using New Line \n
+# print("\nUsing The New Line Format")
+# print("Hello World, \nHello World, \nHello World")
+
+#Reading User Input
+# print("\nReading User Input.")
+# input("How many percentage of Shege would you like to see Today Comrade?: ")
+# print("\nHello, " + input("What is your name? "))
+
+#Print out the length of any name(String)
+# Name = input("\nWhat's your name? ")
+# print(len(Name))
+
+#Write a program that switches the values of the variables stored in a and b
+
+#a = input("a: ")
+#b = input("b: ")
+
+#c = a
+#a = b
+#b = c
+
+#print("a = " + a)
+# print("b = " + b)
+
+#Band Name Generator
+#1. Create a greeting for your program.
+#2. Ask the user for the ciy they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their brand name.
+#5. Make sure the input cursor shows on a new line.
+
+print("Hi, Welcome to the Band Name Genrator Program.\n")
+city = input("Which city did you grow up in? \n")
+pet = input("What was/is the name of your favourite pet? \n")
+print("The name of your band is " + city + " " + pet )
