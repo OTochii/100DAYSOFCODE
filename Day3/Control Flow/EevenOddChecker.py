20bd5c36788e9f1e63cf9e8dbf4e8deccda215ac
Tochee <onyemstochi3@gmail.com>
2023-07-20 21:47:07 +0100

    Delete Day3/Control Flow directory

--- Day3/Control Flow/EevenOddChecker.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Which number do you want to check? "))
-
-if number % 2 == 1:
-    print(str(number))
-    print("number is an odd number.")
-else:
-    print(str(number))
-    print("number is an even number.")
